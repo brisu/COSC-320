c84705cdb83515c66a565b8c2854f40792d4a8ef
brisu <brisu@alumni.ubc.ca>
2020-04-05 21:04:02 +0800

    Testing new sudo

--- LCS.py
+++ LCS.py
@@ -1,34 +1,64 @@
-#Naive method
-def LCSTable(x, y,m, n):
+from array import *
 
-    m = len()
-    it = iter(x)
-    for y in it:
-        print(x)
-        break
+#first formulate the table of content
+def LCSTable(x, y):
+    x = "accdcdbdbd"
+    y = "bdbd"
+    # get length of the strings to start populating the LCS table
+    m = len(x)
+    n = len(y)
+    # produce 2D array
+        #table = [[0]*cols]*rows
+    # Alt method for table population
+    table = [[0]*(m+1)]*(n+1)
+    #table = [[None]*(m+1) for i in range(n+1)]
 
-    # return all(c in it for c in y)
+    # set first element of 2d array as 0
+    #table[0][0] = 0
 
-    """
-    # i is start of index of the array x
-    i = 0
-    # i is start of index of the array y
-    j = 0
+    # attempt to set first row of table[1][m] to zero as it represents empty subsequence of y
+    #for i in range(1,m):
+    #   table[0][i] = 0
 
-    while i < m and j < n:
-        if x[j] == y[j]:
-            j = j + 1
-        i = i + 1
+    # attempt to set first column of table[1][m] to zero as it represents empty subsequence of x
+    #for i in range(1 , n):
+    #    table[i][0] = 0
 
-    return j == m
-    print(m)
-    """
+    # table population error, it is populating but not making
+    # the first row full of zeroes
 
-#    it = iter(y)
-#    return all(c in it for c in x)
+    # this method currently doesn't work
+    for i in range(0 , n):
+        for j in range(0 , m):
+            if x[j] == y[i]:
+                table[i+1][j+1] = table[i][j] + 1
+            else:
+                table[i+1][j+1] = max(table[i-1][j], table[i][j-1])
+    # return method returns an error that it is outside function
+    for i in table:
+        print(i)
+    return table[n][m]
 
-    text = "thisisatestwithinatestaha"
-    pattern = "test"
-    m = len(text)
-    n = len(pattern)
-    LCSTable(text,patter,m,n)
+def printLCS(table, x, y):
+    S = []
+    m = len(x) # i
+    n = len(y) # j
+    while m != 0 and n != 0:
+        if x[m-1] == y[n-1]:
+            S.push(x[m-1])
+            m = m - 1
+            n = n -1
+        elif table[m-1][n] == table [m][n-1]:
+            m = m - 1
+        else:
+            n = n - 1
+    while S:
+        print(S.pop)
+
+test = "hellothisisatestfortheLCSSalgorithm"
+x = "accdcdbdbd"
+y = "bdbd"
+pattern = "hellothisatestfortheKMPalgrothmi"
+LCSTable(x,y)
+
+printLCS(LCSTable(x,y), x, y)
