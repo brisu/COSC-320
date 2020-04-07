52dd173ccdba3a6c5fe6f8d5d697c074ee32a663
brisu <brisu@alumni.ubc.ca>
2020-04-07 21:51:05 +0800

    Changes?

--- KMP.py
+++ KMP.py
@@ -2,52 +2,52 @@ tocheck = "palgiarizing abacac palgiarizing plagiarized document ohh nono."
 pattern = "plagiarizing plagiarized document"
 IPS = []
 
-def makeIPS(): 
-    #initializing the IPS 
+def makeIPS():
+    #initializing the IPS
     for i in range(len(pattern)+1):
         IPS.append(0)
     prefix = [pattern[0]]
-    
+
     num = 1
-    increment = 0    
+    increment = 0
     for i in range( 1, len(pattern) ):
-        
+
         if ( pattern[i] == prefix[(0 + increment)] ):
             IPS[(i+1)] = num
             num = num + 1
             increment = increment + 1
             prefix.append(pattern[increment])
         else:
-            
+
             prefix.clear()
             prefix.append(pattern[0])
-            
+
             num = 1
-            increment = 0    
-        
+            increment = 0
+
 
 
 def didYouPlagiarize():
     j = 0
     start = 0
-    end = 0 
+    end = 0
     for i in range(len(tocheck)):
         if tocheck[i] == pattern[(j + 1)]:
             j = j + 1
             if j == len(pattern)-1:
-                start = i - len(pattern) +1 
+                start = i - len(pattern) +1
                 end = i
-                j = 0    
+                j = 0
         else:
-            gobackpoint = IPS[j] 
+            gobackpoint = IPS[j]
             if gobackpoint != 0:
                 i = i-1
                 j = gobackpoint
-    if ( start != 0 and end != 0 ) :            
+    if ( start != 0 and end != 0 ) :
         print("Start the Same From Location:",start," | End at the Location:", end)
         print("On the file:" , tocheck[start:(end+1)])
         f.write("Start the Same From Location: "+ str(start) +" | End at the Location: " + str(end) + '\n' )
-        f.write("The sentance is: " + tocheck[start:(end+1)] + '\n')          
+        f.write("The sentance is: " + tocheck[start:(end+1)] + '\n')
 
 oneP=[]
 f1 = open( "original.txt" , "r" )
@@ -67,12 +67,7 @@ for x in f1:
                 # print("The Pi Cahrt/ IPS: " , IPS)
                 didYouPlagiarize()
     else:
-        oneP.clear()        
+        oneP.clear()
 f1.close()
 f2.close()
 f.close()
-
-
-    
-    
-
